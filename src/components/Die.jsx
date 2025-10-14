0b711fa0bc4648ce062d83b8a6a7309d83deaaf2
Essam <shahd.essam.ext@siemens.com>
2025-10-15 02:12:15 +0300

    Add game screenshots

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,12 +3,12 @@ export default function Die(props) {
     backgroundColor: props.isHeld ? "#59E391" : "white",
   };
   return (
-    <div
+    <button
       style={styles}
       className="size-[80px] p-5 bg-white flex justify-center items-center rounded-md cursor-pointer shadow-md"
       onClick={props.holdDie}
     >
       <span className="text-gray-800 font-bold text-[30px]">{props.value}</span>
-    </div>
+    </button>
   );
 }
